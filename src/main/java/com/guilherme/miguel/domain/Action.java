e742beb3f4d35d31d12c896c629cd8e471ae9445
Miguel Guilherme <mguilherme82@gmail.com>
2016-10-31 21:31:48 +0000

    Comment setters for createdDate and lastModifiedDate

--- src/main/java/com/guilherme/miguel/domain/Action.java
+++ src/main/java/com/guilherme/miguel/domain/Action.java
@@ -50,15 +50,15 @@ public class Action {
         return createdDate;
     }
 
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
+//    public void setCreatedDate(Date createdDate) {
+//        this.createdDate = createdDate;
+//    }
 
     public Date getLastModifiedDate() {
         return lastModifiedDate;
     }
 
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
+//    public void setLastModifiedDate(Date lastModifiedDate) {
+//        this.lastModifiedDate = lastModifiedDate;
+//    }
 }
